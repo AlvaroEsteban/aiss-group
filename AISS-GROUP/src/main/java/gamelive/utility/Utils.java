321ea70ae3c5e1a9653d44584fb27f84e466c36a
Alvaro Esteban <alvarorota2011@gmail.com>
2018-05-20 20:59:40 +0200

    Incluido clase de utils

---
+++ AISS-GROUP/src/main/java/gamelive/utility/Utils.java
@@ -0,0 +1,10 @@
+package gamelive.utility;
+
+public class Utils {
+
+	public static boolean isBlank(String string){
+		if(string == null || string.isEmpty())
+			return true;
+		return false;
+	}
+}
