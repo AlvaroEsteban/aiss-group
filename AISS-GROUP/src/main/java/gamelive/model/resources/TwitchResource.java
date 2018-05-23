67b8b85995f3be105e21a4cd20454601a8ce37df
Antonio <USUARIO@DESKTOP-PB8K0ED.us.es>
2018-05-23 17:56:09 +0200

    Actualizada

--- AISS-GROUP/src/main/java/gamelive/model/resources/TwitchResource.java
+++ AISS-GROUP/src/main/java/gamelive/model/resources/TwitchResource.java
@@ -13,19 +13,6 @@ import gamelive.model.twitch.StreamSearch;
 
 public class TwitchResource {
 	private String client_id = "pbfpd1osbjx3ytibh3gloui8f5lnhd";
-	
-	
-		/*public StreamSearch getAllStreams() {
-			ClientResource cr = null;
-			StreamSearch streams = null;
-			try {
-				cr = new ClientResource(uri + "?client_id=" + client_id);
-				streams = cr.get(StreamSearch.class);
-			} catch (ResourceException re) {
-				System.err.println("Error al consultar los directos: " + cr.getResponse().getStatus());
-			}
-			return streams;
-		}*/
 		public StreamSearch getStreamsByGame(String game) throws UnsupportedEncodingException {
 			ClientResource cr = null;
 			String uri = "https://api.twitch.tv/kraken/search/streams?query=";
