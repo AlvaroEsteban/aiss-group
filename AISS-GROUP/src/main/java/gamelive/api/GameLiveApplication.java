1fd72c9ea1d519cd1e0198cbd150be2f8864c6ec
Alvaro Esteban <alvarorota2011@gmail.com>
2018-05-27 23:20:55 +0200

    changelog y pom

--- AISS-GROUP/src/main/java/gamelive/api/GameLiveApplication.java
+++ AISS-GROUP/src/main/java/gamelive/api/GameLiveApplication.java
@@ -5,12 +5,18 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import gamelive.api.resources.GameResource;
+import gamelive.api.resources.NoticiaResource;
+import gamelive.api.resources.SubscribeListResource;
+
 public class GameLiveApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
 	private Set<Class<?>> classes = new HashSet<Class<?>>();
 	
 	public GameLiveApplication() {
-
+		singletons.add(GameResource.getInstance());
+		singletons.add(SubscribeListResource.getInstance());
+		singletons.add(NoticiaResource.getInstance());
 	}
 	
 	@Override
